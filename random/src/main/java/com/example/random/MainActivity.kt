55becd0aa68e77360746960513b481cf6ee300cd
Lior <90lior90@gmail.com>
2019-08-04 17:47:03 +0300

    4.8

--- random/src/main/java/com/example/random/MainActivity.kt
+++ random/src/main/java/com/example/random/MainActivity.kt
@@ -2,11 +2,104 @@ package com.example.random
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import android.view.animation.LinearInterpolator
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+import java.security.acl.LastOwnerException
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    var count = 0
+    var  sequence = arrayListOf<Int>()
+    var answerSequence = arrayListOf<Int>()
+    var failed= false
+    var gameStarted = false
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
+
+
+
+    }
+
+    fun checkAnswer(buttonNumber: Int) {
+        if (gameStarted) {
+            answerSequence.add(buttonNumber)
+            for (i in 0 until answerSequence.size) {
+                if (answerSequence[i] != sequence[i]) {
+                    game_tv.text = "Level: ${sequence.size}, Failed!!"
+                    failed = true
+                    gameStarted = false
+                    answerSequence.removeAll(answerSequence)
+                }
+            }
+
+
+            if (answerSequence.size == sequence.size && !failed) {
+                game_tv.text = "Level has completed! Welcome to level: ${sequence.size + 1}!"
+                answerSequence.removeAll(answerSequence)
+                var random = Random.nextInt(4)
+                sequence.add(random)
+                gameStarted = false
+            }
+
+
+        }
+
+    }
+
+    fun showFlick(sequence: ArrayList<Int>) {
+        var handler = Handler()
+        var seconds: Long = 1
+        var isLast = false
+        onOffButtons(false)
+        for (j in sequence) {
+            handler.postDelayed(
+                {
+                    if (j == sequence[sequence.size - 1])
+                        isLast = true
+                    when (j) {
+                        0 -> flickbtn(blackbtn, isLast)
+                        1 -> flickbtn(redbtn, isLast)
+                        2 -> flickbtn(bluebtn, isLast)
+                        3 -> flickbtn(yellowbtn, isLast)
+                    }
+                },(1000 * seconds))
+                        seconds++
+        }
+    }
+
+
+    fun flickbtn(button: View, isLast: Boolean) {
+        val mAnimation = AlphaAnimation(1f, 0f)
+        mAnimation.duration = 500
+        mAnimation.interpolator = LinearInterpolator()
+        mAnimation.repeatCount = 1
+        mAnimation.repeatMode = Animation.REVERSE
+        button.startAnimation(mAnimation)
+        if (isLast)
+            onOffButtons(true)
+
+    }
+
+    fun onOffButtons (isEnable: Boolean){
+        blackbtn.isEnabled = isEnable
+        redbtn.isEnabled = isEnable
+        bluebtn.isEnabled = isEnable
+        yellowbtn.isEnabled = isEnable
     }
 }
+
+
+
